862e9248b89ccc6739a5dc5b381afd2cf47b9f47
Sobhan <sobhandanger81@gmail.com>
2023-08-31 15:06:03 +0330

    json validatoin write just with text area and zod and tests completed

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -5,28 +5,31 @@ import { z } from "zod";
 
 const validationSchema = z
   .object({
-    firstName: z
-      .string()
-      .min(3, { message: "First name must be at least 3 characters" }),
-    lastName: z
-      .string()
-      .min(4, { message: "Last name must be at least 4 characters" }),
-    email: z.string().min(1, { message: "Email required" }).email({
-      message: "Must be a valid email",
-    }),
-    password: z
-      .string()
-      .min(6, { message: "Password must be at least 6 characters" }),
-    confirmPassword: z
-      .string()
-      .min(6, { message: "Password must be at least 6 characters" }),
-    terms: z.literal(true, {
-      errorMap: () => ({ message: "You must accept Terms and Conditions" }),
-    }),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    path: ["confirmPassword"],
-    message: "Password don't match",
+    // firstName: z
+    //   .string()
+    //   .min(3, { message: "First name must be at least 3 characters" }),
+    // lastName: z
+    //   .string()
+    //   .min(4, { message: "Last name must be at least 4 characters" }),
+    email: z.string().min(1, { message: "Email required" }),
+    // password: z
+    //   .string()
+    //   .min(6, { message: "Password must be at least 6 characters" }),
+    // confirmPassword: z
+    //   .string()
+    //   .min(6, { message: "Password must be at least 6 characters" }),
+    // terms: z.literal(true, {
+    //   errorMap: () => ({ message: "You must accept Terms and Conditions" }),
+    // }),
+  }).refine((data) =>{try {
+  JSON.parse(data.email);
+}catch (err){
+      return false;}
+
+  return true;
+  }, {
+    path: ["email"],
+    message: "not correct json format",
   });
 type ValidationSchema = z.infer<typeof validationSchema>;
 
@@ -41,7 +44,7 @@ const Forms: React.FC = () => {
   });
 
   const submitFunction: SubmitHandler<ValidationSchema> = (data) => {
-    console.log(data);
+    console.log(JSON.parse(data.email));
   };
 
   return (
@@ -50,48 +53,48 @@ const Forms: React.FC = () => {
         className="w-1/4 shadow-lg flex items-center justify-center flex-col px-8 pt-6 pb-8 mb-4"
         onSubmit={handleSubmit(submitFunction)}
       >
-        <div className="mb-4 md:flex md:justify-between">
-          <div className="mb-4 md:mr-2 md:mb-0">
-            <label
-              className="block mb-2 text-sm font-bold text-gray-700"
-              htmlFor="firstName"
-            >
-              First Name
-            </label>
-            <input
-              className="shadow-md w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
-              id="firstName"
-              type="text"
-              placeholder="First Name"
-              {...register("firstName")}
-            />
-            {errors.firstName && (
-              <p className="text-xs italic text-red-500 mt-2">
-                {errors.firstName?.message}
-              </p>
-            )}
-          </div>
-          <div className="md:ml-2">
-            <label
-              className="block mb-2 text-sm font-bold text-gray-700"
-              htmlFor="lastName"
-            >
-              Last Name
-            </label>
-            <input
-              className="shadow-md w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
-              id="lastName"
-              type="text"
-              placeholder="Last Name"
-              {...register("lastName")}
-            />
-            {errors.lastName && (
-              <p className="text-xs italic text-red-500 mt-2">
-                {errors.lastName?.message}
-              </p>
-            )}
-          </div>
-        </div>
+        {/*<div className="mb-4 md:flex md:justify-between">*/}
+        {/*  <div className="mb-4 md:mr-2 md:mb-0">*/}
+        {/*    <label*/}
+        {/*      className="block mb-2 text-sm font-bold text-gray-700"*/}
+        {/*      htmlFor="firstName"*/}
+        {/*    >*/}
+        {/*      First Name*/}
+        {/*    </label>*/}
+        {/*    <input*/}
+        {/*      className="shadow-md w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"*/}
+        {/*      id="firstName"*/}
+        {/*      type="text"*/}
+        {/*      placeholder="First Name"*/}
+        {/*      {...register("firstName")}*/}
+        {/*    />*/}
+        {/*    {errors.firstName && (*/}
+        {/*      <p className="text-xs italic text-red-500 mt-2">*/}
+        {/*        {errors.firstName?.message}*/}
+        {/*      </p>*/}
+        {/*    )}*/}
+        {/*  </div>*/}
+        {/*  <div className="md:ml-2">*/}
+        {/*    <label*/}
+        {/*      className="block mb-2 text-sm font-bold text-gray-700"*/}
+        {/*      htmlFor="lastName"*/}
+        {/*    >*/}
+        {/*      Last Name*/}
+        {/*    </label>*/}
+        {/*    <input*/}
+        {/*      className="shadow-md w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"*/}
+        {/*      id="lastName"*/}
+        {/*      type="text"*/}
+        {/*      placeholder="Last Name"*/}
+        {/*      {...register("lastName")}*/}
+        {/*    />*/}
+        {/*    {errors.lastName && (*/}
+        {/*      <p className="text-xs italic text-red-500 mt-2">*/}
+        {/*        {errors.lastName?.message}*/}
+        {/*      </p>*/}
+        {/*    )}*/}
+        {/*  </div>*/}
+        {/*</div>*/}
         <div className="mb-4 w-full px-2">
           <label
             className="block mb-2 text-sm font-bold text-gray-700 mx-2"
@@ -99,7 +102,7 @@ const Forms: React.FC = () => {
           >
             Email
           </label>
-          <input
+          <textarea
             className="shadow-md w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline "
             id="email"
             type="email"
@@ -112,60 +115,60 @@ const Forms: React.FC = () => {
             </p>
           )}
         </div>
-        <div className="mb-4 md:flex md:justify-between">
-          <div className="mb-4 md:mr-2 md:mb-0">
-            <label
-              className="block mb-2 text-sm font-bold text-gray-700"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="shadow-md w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              {...register("password")}
-            />
-            {errors.password && (
-              <p className="text-xs italic text-red-500 mt-2">
-                {errors.password?.message}
-              </p>
-            )}
-          </div>
-          <div className="md:ml-2">
-            <label
-              className="block mb-2 text-sm font-bold text-gray-700"
-              htmlFor="c_password"
-            >
-              Confirm Password
-            </label>
-            <input
-              className="shadow-md w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
-              id="c_password"
-              type="password"
-              {...register("confirmPassword")}
-            />
-            {errors.confirmPassword && (
-              <p className="text-xs italic text-red-500 mt-2">
-                {errors.confirmPassword?.message}
-              </p>
-            )}
-          </div>
-        </div>
-        <div className="mb-4">
-          <input type="checkbox" id="terms" {...register("terms")} />
-          <label
-            htmlFor="terms"
-            className="ml-2 mb-2 text-sm font-bold text-gray-700"
-          >
-            Accept Terms & Conditions
-          </label>
-          {errors.terms && (
-            <p className="text-xs italic text-red-500 mt-2">
-              {errors.terms?.message}
-            </p>
-          )}
-        </div>
+        {/*<div className="mb-4 md:flex md:justify-between">*/}
+        {/*  <div className="mb-4 md:mr-2 md:mb-0">*/}
+        {/*    <label*/}
+        {/*      className="block mb-2 text-sm font-bold text-gray-700"*/}
+        {/*      htmlFor="password"*/}
+        {/*    >*/}
+        {/*      Password*/}
+        {/*    </label>*/}
+        {/*    <input*/}
+        {/*      className="shadow-md w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"*/}
+        {/*      id="password"*/}
+        {/*      type="password"*/}
+        {/*      {...register("password")}*/}
+        {/*    />*/}
+        {/*    {errors.password && (*/}
+        {/*      <p className="text-xs italic text-red-500 mt-2">*/}
+        {/*        {errors.password?.message}*/}
+        {/*      </p>*/}
+        {/*    )}*/}
+        {/*  </div>*/}
+        {/*  <div className="md:ml-2">*/}
+        {/*    <label*/}
+        {/*      className="block mb-2 text-sm font-bold text-gray-700"*/}
+        {/*      htmlFor="c_password"*/}
+        {/*    >*/}
+        {/*      Confirm Password*/}
+        {/*    </label>*/}
+        {/*    <input*/}
+        {/*      className="shadow-md w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"*/}
+        {/*      id="c_password"*/}
+        {/*      type="password"*/}
+        {/*      {...register("confirmPassword")}*/}
+        {/*    />*/}
+        {/*    {errors.confirmPassword && (*/}
+        {/*      <p className="text-xs italic text-red-500 mt-2">*/}
+        {/*        {errors.confirmPassword?.message}*/}
+        {/*      </p>*/}
+        {/*    )}*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+        {/*<div className="mb-4">*/}
+        {/*  <input type="checkbox" id="terms" {...register("terms")} />*/}
+        {/*  <label*/}
+        {/*    htmlFor="terms"*/}
+        {/*    className="ml-2 mb-2 text-sm font-bold text-gray-700"*/}
+        {/*  >*/}
+        {/*    Accept Terms & Conditions*/}
+        {/*  </label>*/}
+        {/*  {errors.terms && (*/}
+        {/*    <p className="text-xs italic text-red-500 mt-2">*/}
+        {/*      {errors.terms?.message}*/}
+        {/*    </p>*/}
+        {/*  )}*/}
+        {/*</div>*/}
         <div className="mb-6 text-center">
           <button
             className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
